3d24ba48c6cdbebcfb0b19af291cfd0213597dc1
loich79 <loic.hay79@gmail.com>
2017-10-11 20:22:09 +0200

    ajout des principales routes et des vues qui leurs sont associées + mise a jour de base.html.twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,94 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction()
+    {
+        return $this->render("default/dashboard.html.twig");
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/landing-page", name="landingPage")
+     */
+    public function landingPageAction()
+    {
+        return $this->render("default/landingPage.html.twig");
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/saisie-observation", name="saisieObservation")
+     */
+    public function addObservationAction()
+    {
+        return $this->render("default/addObservation.html.twig");
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/recherche-observations", name="rechercheObservations")
+     */
+    public function searchObservationsAction()
+    {
+        return $this->render("default/searchObservations.html.twig");
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/fiche-espece/{species}", name="ficheEspece")
+     */
+    public function speciesAction($species)
+    {
+        return $this->render("default/species.html.twig", array('species' => $species));
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/blog", name="indexBlog")
+     */
+    public function indexBlogAction()
+    {
+        return $this->render("default/blog/indexBlog.html.twig");
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/blog/{post}", name="postBlog")
+     */
+    public function postBlogAction($post)
+    {
+        return $this->render("default/blog/postBlog.html.twig", array('post' => $post));
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/blog/categorie/{category}", name="categoryBlog")
+     */
+    public function categoryBlogAction($category)
+    {
+        return $this->render("default/blog/categoryBlog.html.twig", array('category' => $category));
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/avancee-recherche", name="avanceeRecherche")
+     */
+    public function researchAction()
+    {
+        return $this->render("default/research.html.twig");
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/faq", name="faq")
+     */
+    public function faqAction()
+    {
+        return $this->render("default/faq.html.twig");
+    }
 }
