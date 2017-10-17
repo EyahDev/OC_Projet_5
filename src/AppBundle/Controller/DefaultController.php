a8904ce8ce19e2d63e2e5c77935b7de0480df6fe
Dumas Chaumette <dumas-chaumette@live.fr>
2017-10-17 13:11:02 +0200

    Ajout de la gestion des informations personnelles

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Services\BlogManager;
+use AppBundle\Services\InformationManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
