fc0b909b3fd8cdabed13adbceb77a5e71ab03fa4
loich79 <loic.hay79@gmail.com>
2017-10-23 16:10:17 +0200

    Correction des bugs d'affichage sur le back office de la FAQ

--- web/js/faq/faq.js
+++ web/js/faq/faq.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     // fonction permettant d'ajouter une ligne au tableau de FAQ après la soumission du formulaire d'ajout
     function addFaq(data, form) {
-        console.log(data);
         $('#tableFaq').children('tbody').append(data);
         $('.btn-add-faq').fadeIn( "fast", function() {
         });
@@ -20,12 +19,83 @@ $(document).ready(function() {
                 success: function (data) {
                     // efface la ligne du tableau correspondant à la question/réponse supprimée
                     $a.parentsUntil('tbody').replaceWith("");
+                    // ajoute le message flash
                     addFlashMsg('success', data);
                 },
                 error: function (jqxhr) {
                     addFlashMsg('danger', "Une erreur est survenue")
                 }
             })
+        });
+    }
+
+    // fonction permettant d'ajouter le traitement de la modification après un ajout
+    // ou une modification d'une question/réponse
+    function editFaq() {
+        $('.btn-edit-faq').on('click', function edit(e){
+            e.preventDefault();
+            var $a = $(this);
+            var url = $a.attr('href');
+            $.ajax({
+                type: 'GET',
+                url: url,
+                success: function(data) {
+                    // ajoute la modale d'edition
+                    $a.parent().prepend(data);
+                    // affiche la modale d'édition
+                    $a.prev().modal('show');
+                    // en cas de fermeture  de la modale sans modification
+                    $a.prev().on('hidden.bs.modal', function (e) {
+                        // supprime la modale d'édition
+                        $a.prev().replaceWith('');
+                    });
+                    // A la soumission du formulaire d'édition
+                    $('form[name="edit_faq"]').on('submit', function(e){
+                        e.preventDefault();
+                        var $form = $(this);
+                        url = $a.attr('href');
+                        $.ajax({
+                            type: 'POST',
+                            url: url,
+                            data: $form.serialize(),
+                            success: function (data, text, jqxhr) {
+                                // ajoute un message flash
+                                var appendCode = '<div class="flash-msg alert alert-success">Question modifiée</div>';
+                                $form.parent().prepend(appendCode);
+                                // efface le message flash apres 5 secondes
+                                function removeFlagMsg(){
+                                    $('.flash-msg').replaceWith("");
+                                }
+                                setTimeout(removeFlagMsg, 5000);
+                                // a la fermeture de la modale
+                                $a.prev().on('hidden.bs.modal', function (e) {
+                                    // efface la modale d'édition
+                                    $a.prev().replaceWith('');
+                                    // modifie la ligne du tableau qui vient d'etre modifié
+                                    $a.parentsUntil('tbody').replaceWith(data);
+                                    // retire l'ecoute de l'évenement clic sur un bouton edit
+                                    $('.btn-edit-faq').off('click', edit);
+                                    removeFaq();
+                                    editFaq();
+                                });
+                            },
+                            error: function (jqxhr) {
+                                var appendCode = '<div class="flash-msg alert alert-danger">Le formulaire comporte des erreurs (la question et la réponse doivent comporter au moins 2 caractères)</div>';
+                                $form.parent().prepend(appendCode);
+                                // efface le message flash apres 5 secondes
+                                function removeFlagMsg(){
+                                    $('.flash-msg').replaceWith("");
+                                }
+                                setTimeout(removeFlagMsg, 5000);
+                            }
+                        })
+                    })
+
+                },
+                error: function() {
+                    addFlashMsg('danger', "Une erreur est survenue")
+                }
+            });
         });
     }
 
@@ -51,9 +121,11 @@ $(document).ready(function() {
             type: 'GET',
             url: url,
             success: function(data) {
+                // modifie la div contenant le bouton ajouter une question
                 $a.parent().removeClass("mx-auto");
                 $a.parent().prepend(data);
                 $a.hide();
+                // a la soumission du formulaire
                 $('form[name="new_faq"]').on('submit', function(e){
                     e.preventDefault();
                     var $form = $(this);
@@ -65,7 +137,10 @@ $(document).ready(function() {
                         success: function (data, text, jqxhr) {
                             addFaq(data, $form);
                             addFlashMsg('success', "Question ajoutée");
+                            // retire l'ecoute de l'évenement clic sur un bouton edit
+                            $('.btn-edit-faq').off('click');
                             removeFaq();
+                            editFaq();
                         },
                         error: function() {
                             addFlashMsg('danger', "Le formulaire comporte des erreurs (la question et la réponse doivent comporter au moins 2 caractères)")
@@ -98,7 +173,7 @@ $(document).ready(function() {
         })
     });
     // EDITION D'UNE QUESTION/REPONSE
-    $('.btn-edit-faq').on('click', function(e){
+    $('.btn-edit-faq').on('click', function edit(e){
         e.preventDefault();
         var $a = $(this);
         var url = $a.attr('href');
@@ -107,7 +182,15 @@ $(document).ready(function() {
             type: 'GET',
             url: url,
             success: function(data) {
-                $('.show').children().children().children('.modal-body').append(data);
+                // ajoute la modale d'édition
+                $a.parent().prepend(data);
+                // affiche la modale d'édition
+                $a.prev().modal('show');
+                // en cas de fermeture de la modale sans enregistrer
+                $a.prev().on('hidden.bs.modal', function (e) {
+                    // supprime la modale d'édition
+                     $a.prev().replaceWith('');
+                });
                 $('form[name="edit_faq"]').on('submit', function(e){
                     e.preventDefault();
                     var $form = $(this);
@@ -117,6 +200,7 @@ $(document).ready(function() {
                         url: url,
                         data: $form.serialize(),
                         success: function (data, text, jqxhr) {
+                            // ajoute un message flash
                             var appendCode = '<div class="flash-msg alert alert-success">Question modifiée</div>';
                             $form.parent().prepend(appendCode);
                             // efface le message flash apres 5 secondes
@@ -124,10 +208,16 @@ $(document).ready(function() {
                                 $('.flash-msg').replaceWith("");
                             }
                             setTimeout(removeFlagMsg, 5000);
-                            $form.parentsUntil('.card-body').on('hidden.bs.modal', function (e) {
-                                $form.replaceWith("");
+                            // a la fermuture de la modale
+                            $a.prev().on('hidden.bs.modal', function (e) {
+                                // efface la modale
+                                $a.prev().replaceWith('');
+                                // modifie la ligne correspondant a la question/réponse modifiée
                                 $a.parentsUntil('tbody').replaceWith(data);
+                                // supprime l'écoute sur l'evenement clic sur le bouton d'edition
+                                $('.btn-edit-faq').off('click', edit);
                                 removeFaq();
+                                editFaq();
                             });
                         },
                         error: function (jqxhr) {
@@ -148,4 +238,6 @@ $(document).ready(function() {
             }
         });
     });
+
+
 });
