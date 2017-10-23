162fc212a7710fbbe1c10f634f7532c20687787c
Dumas Chaumette <dumas-chaumette@live.fr>
2017-10-23 11:42:42 +0200

    Commencement intégration layout et page d'accueil

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('layout/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
